9b1bc1aa63e252f1a52f2b652fc671fcc331b539
dzulia-2005 <159149786+dzulia-2005@users.noreply.github.com>
2025-11-14 11:19:42 +0400

    first commit

---
+++ backend/Models/Stats.cs
@@ -0,0 +1,9 @@
+namespace backend.Models;
+
+public class Stats
+{
+    public int Id { get; set; }
+    public int Value { get; set; }
+    public int Version { get; set; }
+    public DateTime UpdateAt { get; set; }
+}
---
+++ backend/Program.cs
@@ -0,0 +1,22 @@
+using backend.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => 
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
+
+
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+app.Run();
