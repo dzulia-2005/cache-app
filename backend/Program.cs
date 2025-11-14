11d82aea888cb74606595bac6eb22cefbd22f1c5
dzulia-2005 <159149786+dzulia-2005@users.noreply.github.com>
2025-11-14 11:51:19 +0400

    feat: add PostgreSQL, Redis and EF Core packages

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,22 +1,37 @@
 using backend.Data;
 using Microsoft.EntityFrameworkCore;
+using StackExchange.Redis;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options => 
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
 
+builder.Services.AddSingleton<IConnectionMultiplexer>(
+    ConnectionMultiplexer.Connect(builder.Configuration["Redis:Connection"])
+);
 
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration["Redis:Connection"];
+});
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
+
+app.MapControllers();
 app.UseHttpsRedirection();
 app.Run();
